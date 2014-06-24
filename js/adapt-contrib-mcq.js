2c501d49e4bbf2e4b514052168f0adfdf860ee8c
Daryl Hedley <darylhedley@hotmail.com>
2014-06-24 09:29:22 +0100

    Updated to new questionView

--- js/adapt-contrib-mcq.js
+++ js/adapt-contrib-mcq.js
@@ -7,126 +7,55 @@ define(function(require) {
         events: {
             'focus .mcq-item input':'onItemFocus',
             'blur .mcq-item input':'onItemBlur',
-            'change .mcq-item input':'onItemSelected',
-            "click .mcq-widget .button.submit": "onSubmitClicked",
-			"click .mcq-widget .button.reset": "onResetClicked",
-			"click .mcq-widget .button.model": "onModelAnswerClicked",
-			"click .mcq-widget .button.user": "onUserAnswerClicked"
+            'change .mcq-item input':'onItemSelected'
         },
 
-        initialize:function() {
-            QuestionView.prototype.initialize.apply(this, arguments);
+        resetQuestionOnRevisit: function() {
+            this.setAllItemsEnabled(true);
+            this.resetQuestion();
+        },
 
+        // Left blank for question setup
+        setupQuestion: function() {
+            // Set selectedItem array
             this.model.set('_selectedItems', []);
-        },
-
-        preRender:function(){
-            QuestionView.prototype.preRender.apply(this);
-
+            // Check if items need to be randomised
             if (this.model.get('_isRandom') && this.model.get('_isEnabled')) {
                 this.model.set("_items", _.shuffle(this.model.get("_items")));
             }
+            
         },
 
-        postRender: function() {
-            QuestionView.prototype.postRender.apply(this);
-            this.setResetButtonEnabled(false);
-            this.setReadyStatus();
+        disableQuestion: function() {
+            this.setAllItemsEnabled(false);
         },
 
-        resetQuestion: function(properties) {
-        	QuestionView.prototype.resetQuestion.apply(this, arguments);
-
-        	_.each(this.model.get('_items'), function(item) {
-                item.selected = false;
-            }, this);
-        },
-
-        canSubmit: function() {
-            return this.getNumberOfOptionsSelected() > 0;
-        },
-
-        canReset: function() {
-            return !this.$('.mcq-widget, .button.reset').hasClass('disabled');
-        },
-
-        forEachAnswer: function(callback) {
-            _.each(this.model.get('_items'), function(item, index) {
-                var correctSelection = item.selected == item._shouldBeSelected;
-                if(item.selected && correctSelection) {
-                	this.model.set('_isAtLeastOneCorrectSelection', true);
-                }
-                callback(correctSelection, item);
-            }, this);
-        },
-
-        markQuestion: function() {
-        	this.forEachAnswer(function(correct, item) {
-        		item.correct = correct;
-        	});
-        	QuestionView.prototype.markQuestion.apply(this);
-        },
-
-        resetItems: function() { 
-            this.$('.mcq-item label').removeClass('selected');
-            this.$('input').prop('checked', false);
-            this.deselectAllItems();
+        enableQuestion: function() {
             this.setAllItemsEnabled(true);
         },
 
-        getNumberOfOptionsSelected:function() {
-        	var count = 0;
-
-        	_.each(this.model.get('_items'), function(item) {
-        		if (item.selected) count++;
-        	}, this);
-
-        	return count;
-        },
-        
-        deselectAllItems: function() {
-            _.each(this.model.get('_items'), function(item) {
-                item.selected = false;
-            }, this);
-        },
-
-        setAllItemsEnabled: function(enabled) {
+        setAllItemsEnabled: function(isEnabled) {
             _.each(this.model.get('_items'), function(item, index){
                 var $itemLabel = this.$('label').eq(index);
                 var $itemInput = this.$('input').eq(index);
 
-                $itemLabel.toggleClass('disabled', !enabled);
-                $itemInput.prop('disabled', !enabled);
+                if (isEnabled) {
+                    $itemLabel.removeClass('disabled');
+                    $itemInput.prop('disabled', false);
+                } else {
+                    $itemLabel.addClass('disabled');
+                    $itemInput.prop('disabled', true);
+                }
             }, this);
         },
 
-        setResetButtonEnabled: function(enabled) {
-            this.$('.button.reset').toggleClass('disabled', !enabled);
+        onQuestionRendered: function() {
+            this.setReadyStatus();
         },
 
-        setOptionSelected:function(index, selected) {
-        	var $itemLabel = this.$('label').eq(index);
-            var $itemInput = this.$('input').eq(index);
-
-            $itemLabel.toggleClass('selected', selected);
-            $itemInput.prop('checked', selected);
-        },
-
-        storeUserAnswer:function() {
-        	var userAnswer = [];
-        	_.each(this.model.get('_items'), function(item, index) {
-        		userAnswer.push(item.selected);
-        	}, this);
-        	this.model.set('_userAnswer', userAnswer);
-        },
-        
-        onItemSelected: function(event) {
-            var selectedItemObject = this.model.get('_items')[$(event.currentTarget).parent('.mcq-item').index()];
-            
-            if(this.model.get('_isEnabled') && !this.model.get('_isSubmitted')){
-                this.toggleItemSelected(selectedItemObject, event);
-            }
-        },
+        //////
+        // Place your interactive code here
+        ////
 
         onItemFocus: function(event) {
             $("label[for='"+$(event.currentTarget).attr('id')+"']").addClass('highlighted');
@@ -134,6 +63,13 @@ define(function(require) {
         
         onItemBlur: function(event) {
             $("label[for='"+$(event.currentTarget).attr('id')+"']").removeClass('highlighted');
+        },
+
+        onItemSelected: function(event) {
+            if(this.model.get('_isEnabled') && !this.model.get('_isSubmitted')){
+                var selectedItemObject = this.model.get('_items')[$(event.currentTarget).parent('.mcq-item').index()];
+                this.toggleItemSelected(selectedItemObject, event);
+            }
         },
 
         toggleItemSelected:function(item, clickEvent) {
@@ -161,39 +97,142 @@ define(function(require) {
                 $itemLabel.removeClass('selected');
             }
             $itemInput.prop('checked', selected);
-            item.selected = selected;
+            item._isSelected = selected;
             this.model.set('_selectedItems', selectedItems);
         },
 
-        onResetClicked: function(event) {
-            if (this.canReset()) {
-                QuestionView.prototype.onResetClicked.apply(this, arguments);
-            } else {
-                if (event) {
-                    event.preventDefault();
+        // Use to check if the user is allowed to submit the question
+        // Maybe the user has to select an item?
+        canSubmit: function() {
+            var count = 0;
+
+            _.each(this.model.get('_items'), function(item) {
+                if (item._isSelected) count++;
+            }, this);
+
+            return (count > 0) ? true : false;
+
+        },
+
+        // Blank method to add functionality for when the user cannot submit
+        // Could be used for a popup or explanation dialog/hint
+        onCannotSubmit: function() {},
+
+        // This is important for returning or showing the users answer
+        // This should preserve the state of the users answers
+        storeUserAnswer: function() {
+            var userAnswer = [];
+            _.each(this.model.get('_items'), function(item, index) {
+                userAnswer.push(item._isSelected);
+            }, this);
+            this.model.set('_userAnswer', userAnswer);
+        },
+
+        isCorrect: function() {
+
+            var numberOfCorrectAnswers = 0;
+
+            _.each(this.model.get('_items'), function(item, index) {
+
+                var itemSelected = (item._isSelected || false);
+
+                if (itemSelected === item._shouldBeSelected) {
+                    numberOfCorrectAnswers ++;
+                    item._isCorrect = true;
+                    this.model.set('_numberOfCorrectAnswers', numberOfCorrectAnswers);
+                    this.model.set('_isAtLeastOneCorrectSelection', true);
+                } else {
+                    item._isCorrect = false;
                 }
+
+            }, this);
+
+            if (numberOfCorrectAnswers === this.model.get('_items').length) {
+                return true;
+            } else {
+                return false;
+            }
+            
+        },
+
+        // Sets the score based upon the questionWeight
+        // Can be overwritten if the question needs to set the score in a different way
+        setScore: function() {
+
+            var numberOfCorrectAnswers = this.model.get('_numberOfCorrectAnswers');
+            var questionWeight = this.model.get("_questionWeight");
+            var itemLength = this.model.get('_items').length;
+
+            var score = questionWeight * numberOfCorrectAnswers / itemLength;
+
+            this.model.set('_score', score);
+
+        },
+
+        // This is important and should give the user feedback on how they answered the question
+        // Normally done through ticks and crosses by adding classes
+        showMarking: function() {
+            _.each(this.model.get('_items'), function(item, i) {
+                var $item = this.$('.mcq-item').eq(i);
+                $item.addClass(item._isCorrect ? 'correct' : 'incorrect');
+            }, this);
+        },
+
+        isPartlyCorrect: function() {
+            return this.model.get('_isAtLeastOneCorrectSelection');
+        },
+
+        resetUserAnswer: function() {
+            this.model.set({_userAnswer: []});
+        },
+
+        // Used by the question view to reset the look and feel of the component.
+        // This could also include resetting item data
+        resetQuestion: function() {
+
+            this.deselectAllItems();
+            this.resetItems();
+
+        },
+
+        deselectAllItems: function() {
+            _.each(this.model.get('_items'), function(item) {
+                item._isSelected = false;
+            }, this);
+        },
+
+        resetItems: function() { 
+            this.$('.mcq-item label').removeClass('selected');
+            this.$('.mcq-item').removeClass('correct incorrect');
+            this.$('input').prop('checked', false);
+            this.model.set({
+                _selectedItems: [],
+                _isAtLeastOneCorrectSelection: false
+            });
+        },
+
+        showCorrectAnswer: function() {
+            _.each(this.model.get('_items'), function(item, index) {
+                this.setOptionSelected(index, item._shouldBeSelected);
+            }, this);
+        },
+
+        setOptionSelected:function(index, selected) {
+            var $itemLabel = this.$('label').eq(index);
+            var $itemInput = this.$('input').eq(index);
+            if (selected) {
+                $itemLabel.addClass('selected');
+                $itemInput.prop('checked', true);
+            } else {
+                $itemLabel.removeClass('selected');
+                $itemInput.prop('checked', false);
             }
         },
 
-        onSubmitClicked: function(event) {
-        	QuestionView.prototype.onSubmitClicked.apply(this, arguments);
-
-            if (this.canSubmit()) {
-               this.setAllItemsEnabled(false);
-               this.setResetButtonEnabled(!this.model.get('_isComplete'));
-            }
-        },
-
-        onModelAnswerShown: function() {
-        	_.each(this.model.get('_items'), function(item, index) {
-        		this.setOptionSelected(index, item._shouldBeSelected);
-        	}, this);
-        },
-
-        onUserAnswerShown: function(event) {
-        	_.each(this.model.get('_items'), function(item, index) {
-        		this.setOptionSelected(index, this.model.get('_userAnswer')[index]);
-        	}, this);
+        hideCorrectAnswer: function() {
+            _.each(this.model.get('_items'), function(item, index) {
+                this.setOptionSelected(index, this.model.get('_userAnswer')[index]);
+            }, this);
         }
 
     });

8c06f0837c0b902c5147e001915b7347051f7697
Brian Quinn <brian@learningpool.com>
2017-08-11 17:00:55 +0100

    Added getInteractionObject() to model
    Also moved getResponseType() and getResponse() functions from view to model.

--- js/mcqView.js
+++ js/mcqView.js
@@ -158,29 +158,7 @@ define([
             _.each(this.model.get('_items'), function(item, index) {
                 this.setOptionSelected(index, this.model.get('_userAnswer')[item._index]);
             }, this);
-        },
-
-        /**
-        * used by adapt-contrib-spoor to get the user's answers in the format required by the cmi.interactions.n.student_response data field
-        * returns the user's answers as a string in the format "1,5,2"
-        */
-        getResponse:function() {
-            var selected = _.where(this.model.get('_items'), {'_isSelected':true});
-            var selectedIndexes = _.pluck(selected, '_index');
-            // indexes are 0-based, we need them to be 1-based for cmi.interactions
-            for (var i = 0, count = selectedIndexes.length; i < count; i++) {
-                selectedIndexes[i]++;
-            }
-            return selectedIndexes.join(',');
-        },
-
-        /**
-        * used by adapt-contrib-spoor to get the type of this question in the format required by the cmi.interactions.n.type data field
-        */
-        getResponseType:function() {
-            return "choice";
         }
-
     });
 
     return McqView;

27104e2096d60b3530c56acac4daa9133c5881d7
Guy Willis <guywillis@users.noreply.github.com>
2019-11-13 16:16:38 +0000

    Release/v5 (#150)
    * Converted tabs to spaces, indentation amends
    
    * Naming refactor
    
    * Updated merge with naming refactor
    
    * Moved positional less to theme, zindex bug fix
    
    * _canShowMarking false icon support
    
    * Bower, example and readme updates
    
    * Removed unnecessary space
    
    * Updated license and cross platform coverage

--- js/adapt-contrib-mcq.js
+++ js/adapt-contrib-mcq.js
@@ -1,14 +1,14 @@
 define([
-    'core/js/adapt',
-    './mcqView',
-    'core/js/models/itemsQuestionModel'
+  'core/js/adapt',
+  './mcqView',
+  'core/js/models/itemsQuestionModel'
 ], function(Adapt, McqView, ItemsQuestionModel) {
 
-    return Adapt.register("mcq", {
-        view: McqView,
-        // Extend ItemsQuestionModel to distinguish McqModel in
-        // the inheritance chain and allow targeted model extensions.
-        model: ItemsQuestionModel.extend({})
-    });
+  return Adapt.register("mcq", {
+    view: McqView,
+    // Extend ItemsQuestionModel to distinguish McqModel in
+    // the inheritance chain and allow targeted model extensions.
+    model: ItemsQuestionModel.extend({})
+  });
 
 });

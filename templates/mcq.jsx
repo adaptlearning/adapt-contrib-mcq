fe0f827d1ad4e860af4df05c06d535748d076d70
Oliver Foster <oliver.foster@kineo.com>
2022-03-22 18:03:39 +0000

    issue/3279 Fix for active item on reset

--- templates/mcq.jsx
+++ templates/mcq.jsx
@@ -59,6 +59,7 @@ export default function Mcq(props) {
               name={_isRadio ? `${_id}-item` : null}
               type={_isRadio ? 'radio' : 'checkbox'}
               disabled={!_isEnabled}
+              checked={_isActive}
               aria-label={!shouldShowMarking ?
                 Adapt.a11y.normalize(text) :
                 `${_shouldBeSelected ? ariaLabels.correct : ariaLabels.incorrect}, ${_isActive ? ariaLabels.selectedAnswer : ariaLabels.unselectedAnswer}. ${Adapt.a11y.normalize(text)}`}

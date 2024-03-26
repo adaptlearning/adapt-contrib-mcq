d06e4e33ebba966dfb027d88dba80636b9db8d44
Lemmy Adams <lemmy.adams@learningpool.com>
2024-03-26 11:16:43 +0000

    Added mcq test

---
+++ test/e2e/mcq.cy.js
@@ -0,0 +1,27 @@
+describe('Multiple Choice Question', function () {
+  beforeEach(function () {
+    cy.getData()
+    cy.visit('/');
+  });
+
+  it('should display the mcq component', function () {
+    const mcqComponents = this.data.components.filter((component) => component._component === 'mcq')
+    mcqComponents.forEach((mcqComponent) => {
+      cy.visit(`/#/preview/${mcqComponent._id}`);
+      const bodyWithoutHtml = mcqComponent.body.replace(/<[^>]*>/g, '');
+
+      cy.testQuestionButtons()
+      cy.testContainsOrNotExists('.mcq__title', mcqComponent.displayTitle)
+      cy.testContainsOrNotExists('.mcq__body', bodyWithoutHtml)
+      cy.testContainsOrNotExists('.mcq__instruction', mcqComponent.instruction)
+      cy.get('.mcq-item').should('have.length', mcqComponent._items.length)
+      mcqComponent._items.forEach((item) => {
+        cy.testContainsOrNotExists('.mcq-item__text', item.text)
+      })
+      
+      // Make sure the current component is tested before moving to the next one
+      // Custom cypress tests are async so we need to wait for them to pass first
+      cy.wait(1000)
+    });
+  });
+});

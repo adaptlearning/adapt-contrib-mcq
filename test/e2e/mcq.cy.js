109328c47c32d6edad67a212964ff005eb59ef93
Lemmy Adams <lemmy.adams@learningpool.com>
2024-03-27 10:53:50 +0000

    Added stripHtml

--- test/e2e/mcq.cy.js
+++ test/e2e/mcq.cy.js
@@ -8,11 +8,12 @@ describe('Multiple Choice Question', function () {
     const mcqComponents = this.data.components.filter((component) => component._component === 'mcq')
     mcqComponents.forEach((mcqComponent) => {
       cy.visit(`/#/preview/${mcqComponent._id}`);
-      const bodyWithoutHtml = mcqComponent.body.replace(/<[^>]*>/g, '');
+      cy.stripHtml(mcqComponent.body)
+      const bodyWithoutHtml = this.text;
+      cy.testContainsOrNotExists('.mcq__body', bodyWithoutHtml)
 
       cy.testQuestionButtons()
       cy.testContainsOrNotExists('.mcq__title', mcqComponent.displayTitle)
-      cy.testContainsOrNotExists('.mcq__body', bodyWithoutHtml)
       cy.testContainsOrNotExists('.mcq__instruction', mcqComponent.instruction)
       cy.get('.mcq-item').should('have.length', mcqComponent._items.length)
       mcqComponent._items.forEach((item) => {

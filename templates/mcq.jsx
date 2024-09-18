46c5a62616b73386d9e155d9160f217a255b5e44
kirsty-hames <kirsty-hames@users.noreply.github.com>
2024-09-18 11:45:07 +0100

    undo aria-disabled and raise as separate issue/pr
    ...change isn't specific to _canShowModelAnswer functionality and will also apply to other question components

--- templates/mcq.jsx
+++ templates/mcq.jsx
@@ -63,7 +63,7 @@ export default function Mcq(props) {
               id={`${_id}-${index}-input`}
               name={_isRadio ? `${_id}-item` : null}
               type={_isRadio ? 'radio' : 'checkbox'}
-              aria-disabled={!_isEnabled}
+              disabled={!_isEnabled}
               checked={_isActive}
               aria-label={!_shouldShowMarking ?
                 a11y.normalize(altText || text) :
